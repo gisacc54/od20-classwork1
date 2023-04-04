429527ae14ea279edcd590a3b7c55a081056c2e2
g54 <giftisacc54@gmail.com>
2023-04-04 11:43:31 +0300

    update

--- app/src/main/java/com/example/classapp/MainActivity.java
+++ app/src/main/java/com/example/classapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.classapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "The onCreate() event", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void testBtn(View v){
+        Toast.makeText(this, "Test BTN", Toast.LENGTH_SHORT).show();
     }
 }
